6ffa5398b53a5adb070c56568e271dc240ec14db
Andy Miller <andy@andyfmiller.com>
2017-12-08 10:53:20 -0800

    Merge sharecontroller into homecontroller

--- gc2lti_catalog/Models/CourseWorkModel.cs
+++ gc2lti_catalog/Models/CourseWorkModel.cs
@@ -1,11 +1,14 @@
-﻿namespace catalog_outcomes.Models
+﻿namespace gc2lti_catalog.Models
 {
-    public class ShareConfirmModel
+    public class CourseWorkModel
     {
         public string CourseId { get; set; }
+        public string CourseAlternativeLink { get; set; }
         public string CourseName { get; set; }
+        public string CourseWorkId { get; set; }
         public string Description { get; set; }
         public int MaxPoints { get; set; }
+        public string PersonName { get; set; }
         public string Title { get; set; }
         public string Url { get; set; }
     }
--- gc2lti_catalog/Models/ShareViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace catalog_outcomes.Models
-{
-    public class ShareViewModel
-    {
-        public string CourseId { get; set; }
-        public string CourseWorkId { get; set; }
-        public string CourseAlternativeLink { get; set; }
-    }
-}
