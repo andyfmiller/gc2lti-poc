d3774876e01402ccf7d6f15eae3f1f8c6e24f249
Andy Miller <andy@andyfmiller.com>
2017-12-11 09:55:57 -0800

    Removed retired projects.

--- gc2lti_catalog/Models/ResourceModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace gc2lti_catalog.Models
-{
-    public class ResourceModel
-    {
-        public string Url { get; set; }
-        public string Title { get; set; }
-        public string Description { get; set; }
-    }
-}
